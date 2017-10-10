87aa3722a0ea6af004d112311f4fcd124beacab8
Vihanga Paranahewa <31098256+VihangaParanahewa@users.noreply.github.com>
2017-10-10 22:21:23 +0530

    15th commit
    develp the viewposts

--- application/views/Customer/ring.php
+++ application/views/Customer/ring.php
@@ -7,6 +7,7 @@
 }
 ?>
 
+
 <div class=" col-md-9 col-lg-9 ">
     <table class="table table-user-information" align="center">
         <tbody>
@@ -21,18 +22,20 @@
         </tr>
 
         <?php foreach ($data as $row){ ?>
-        <tr>
-            <td><?php echo $row['title'];?></td>
-            <td><?php echo $row['category'];?></td>
-            <td><?php echo $row['description'];?></td>
-            <td><?php echo $row['colour'];?></td>
-            <td><?php echo $row['price'];?></td>
-            <td><?php echo $row['image'];?></td>
-        </tr>
+            <tr>
+                <td><?php echo $row['title'];?></td>
+                <td><?php echo $row['category'];?></td>
+                <td><?php echo $row['description'];?></td>
+                <td bgcolor="<?php echo $row['colour'];?>"></td>
+                <td><?php echo $row['price'];?></td>
+                <td><img  src="<?php echo base_url()."image/"; ?><?php  echo  $row['image'];?>" alt="" height="75" width="100"></td>
+            </tr>
         <?php }?>
 
         </tbody>
     </table>
+
+
 
 
 
