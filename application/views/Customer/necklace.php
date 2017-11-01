da53dca6f603bb7c5cabb0d7310727d61ae084f4
Vihanga Paranahewa <31098256+VihangaParanahewa@users.noreply.github.com>
2017-11-01 13:31:48 +0530

    46th commit

--- application/views/Customer/necklace.php
+++ application/views/Customer/necklace.php
@@ -11,37 +11,21 @@
     <table class="table table-user-information" align="center">
         <tbody>
 
-        <tr>
-            <td>Title</td>
-            <td>Category</td>
-            <td>Description</td>
-            <td>Price</td>
-            <td>Image</td>
-            <?php if($this->session->userdata('id')==1){ ?>
-                <td>Action</td>
-            <?php }?>
-        </tr>
-
-        <?php foreach ($data as $row){ ?>
-                <tr>
-                    <td><?php echo $row['title'];?></td>
-                    <td><?php echo $row['category'];?></td>
-                    <td><?php echo $row['description'];?></td>
-                    <td><?php echo $row['price'];?></td>
-                    <td><img  src="<?php echo base_url()."image/"; ?><?php  echo  $row['image'];?>" alt="ringImage" height="150" width="175"></td>
-
-
-                    <?php if($this->session->userdata('id')==1){ ?>
-                        <td>
-                            <a href="<?php echo base_url('index.php/Admin/editNecklacePost/'.$row['necklaceId'])?>"  class="btn btn-info">Edit</a>
-                            <a href="<?php echo base_url('index.php/Admin/deleteNecklace/'.$row['necklaceId'])?>"  onclick="return confirm('Are You Sure ' +
-                         'You Want To Delete Student..!');" class="btn btn-danger">Delete</a>
-                        </td>
-                    <?php }?>
-
-                </tr>
+        <tr><?php foreach ($data as $row){ ?>
+            <td>
+                <img  src="<?php echo base_url()."image/"; ?><?php  echo  $row['image'];?>" alt="necklaceImage" height="200" width="200">
+                <br> Title: <?php echo $row['title'];?> <br>
+                About: <?php echo $row['description'];?> <br>
+                Price: <?php echo $row['price'];?><br>
+                <a href="#" class="btn btn-success">Pay Online</a><br>
+                <?php if($this->session->userdata('id')==1){ ?>
+                <a href="<?php echo base_url('index.php/Admin/editNecklacePost/'.$row['necklaceId'])?>"  class="btn btn-info">Edit</a>
+                <a href="<?php echo base_url('index.php/Admin/deleteNecklace/'.$row['necklaceId'])?>"  onclick="return confirm('Are You Sure ' +
+                         'You Want To Delete Student..!');" class="btn btn-danger">Delete</a><?php }?>
+            </td>
             <?php }
-        ?>
+            ?>
+        </tr>
 
         </tbody>
     </table>
